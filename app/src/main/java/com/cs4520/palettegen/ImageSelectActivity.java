3ec6dc1f2124f66a701661bff9c7f6a6afe784f9
Aaron Levine <levine.aa@husky.neu.edu>
2019-07-24 17:02:17 -0400

    upload now working

--- app/src/main/java/com/cs4520/palettegen/ImageSelectActivity.java
+++ app/src/main/java/com/cs4520/palettegen/ImageSelectActivity.java
@@ -1,6 +1,9 @@
 package com.cs4520.palettegen;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -15,8 +18,12 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -31,23 +38,25 @@ import java.util.Locale;
 
 public class ImageSelectActivity extends AppCompatActivity {
 
-    Button cameraButton;
-    Button uploadButton;
-    ImageButton uploadFromURLButton;
-    EditText uploadFromURLEditText;
-    String currentPhotoPath;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_PICK_IMAGE = 2;
-    static final int REQUEST_URL_IMAGE = 3;
+    private EditText uploadFromURLEditText;
+    private String currentPhotoPath;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private static final int REQUEST_PICK_IMAGE = 2;
+
+    private static final int REQUEST_URL_IMAGE = 3;
+
+    private static final int REQUEST_READ_PERMISSIONS = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_select);
 
-        cameraButton = findViewById(R.id.cameraButton);
-        uploadButton = findViewById(R.id.uploadButton);
-        uploadFromURLButton = findViewById(R.id.urlUploadButton);
+        Button cameraButton = findViewById(R.id.cameraButton);
+        Button uploadButton = findViewById(R.id.uploadButton);
+        ImageButton uploadFromURLButton = findViewById(R.id.urlUploadButton);
         uploadFromURLEditText = findViewById(R.id.urlUpload);
 
         // Add onClick listener for the camera button
@@ -56,6 +65,7 @@ public class ImageSelectActivity extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                     // Create the File where the photo should go
@@ -85,16 +95,23 @@ public class ImageSelectActivity extends AppCompatActivity {
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                getIntent.setType("image/*");
+                // Verify that all required contact permissions have been granted.
+                if (ActivityCompat.checkSelfPermission(ImageSelectActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Contacts permissions have not been granted.
+                    Log.i("ImageSelectActivity",
+                            "Read permissions has NOT been granted. Requesting permissions.");
+                    requestReadPermissions();
+                } else {
 
-                Intent pickIntent = new Intent(Intent.ACTION_PICK);
-                pickIntent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-
-                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
-                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
-
-                startActivityForResult(chooserIntent, REQUEST_PICK_IMAGE);
+                    // Contact permissions have been granted. Show the contacts fragment.
+                    Log.i("ImageSelectActivity",
+                            "Read permissions already granted.");
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                    startActivityForResult(intent, REQUEST_PICK_IMAGE);
+                }
             }
         });
 
@@ -106,6 +123,42 @@ public class ImageSelectActivity extends AppCompatActivity {
                 task.execute(uploadFromURLEditText.getText().toString());
             }
         });
+    }
+
+    /**
+     * Requests the Read External Storage permissions.
+     * If the permission has been denied previously, a SnackBar (yum!) will prompt the user to grant the
+     * permission, otherwise it is requested directly.
+     */
+    private void requestReadPermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(ImageSelectActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example, if the request has been denied previously.
+            Log.i("ImageSelectActivity",
+                    "Displaying read permission rationale to provide additional context.");
+
+            // Display a SnackBar with an explanation and a button to trigger the request.
+            Snackbar.make(findViewById(R.id.imageSelectLayout), "PaletteTown needs your permission to read external storage.",
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Confirmed", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat
+                                    .requestPermissions(ImageSelectActivity.this,
+                                            new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE },
+                                            REQUEST_READ_PERMISSIONS);
+                        }
+                    })
+                    .show();
+        } else {
+            // Contact permissions have not been granted yet. Request them directly.
+            ActivityCompat.requestPermissions(ImageSelectActivity.this,
+                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                    REQUEST_READ_PERMISSIONS);
+        }
     }
 
     /**
@@ -145,7 +198,21 @@ public class ImageSelectActivity extends AppCompatActivity {
         } else if (requestCode == REQUEST_PICK_IMAGE && resultCode == RESULT_OK) {
             Intent moveToPaletteIntent = new Intent(ImageSelectActivity.this, PaletteActivity.class);
 
-            moveToPaletteIntent.putExtra("currentPhotoLocation", currentPhotoPath);
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            assert selectedImage != null;
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+
+            assert cursor != null;
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            moveToPaletteIntent.putExtra("currentPhotoLocation", picturePath);
             startActivity(moveToPaletteIntent);
         } else if (requestCode == REQUEST_URL_IMAGE && resultCode == RESULT_OK) {
             Intent moveToPaletteIntent = new Intent(ImageSelectActivity.this, PaletteActivity.class);
@@ -156,13 +223,44 @@ public class ImageSelectActivity extends AppCompatActivity {
     }
 
     /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_READ_PERMISSIONS) {
+            // Received permission result for read external permission.
+            Log.i("ImageSelectActivity", "Received response for read permission request.");
+
+            // Check if the only required permission has been granted
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Camera permission has been granted, preview can be displayed
+                Log.i("ImageSelectActivity", "READ permission has now been granted. Showing preview.");
+                Snackbar.make(findViewById(R.id.imageSelectLayout), "Permission granted!",
+                        Snackbar.LENGTH_SHORT).show();
+            } else {
+                Log.i("ImageSelectActivity", "READ permission was NOT granted.");
+                Snackbar.make(findViewById(R.id.imageSelectLayout), "Permission not granted.",
+                        Snackbar.LENGTH_SHORT).show();
+
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    /**
      * This AsyncTask is used to download an image from url and then move directly to PaletteActivity
      */
     private class DownloadURLImageTask extends AsyncTask<String, Void, Bitmap> {
-        public int status = OK;
-        static final int OK = 1;
-        static final int IO_ERROR = 2;
-        static final int BAD_URL = 3;
+        private int status = OK;
+
+        private static final int OK = 1;
+
+        private static final int IO_ERROR = 2;
+
+        private static final int BAD_URL = 3;
 
         @Override
         protected Bitmap doInBackground(String... urls) {
--- app/src/main/java/com/cs4520/palettegen/PaletteActivity.java
+++ app/src/main/java/com/cs4520/palettegen/PaletteActivity.java
@@ -1,20 +1,22 @@
 package com.cs4520.palettegen;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.palette.graphics.Palette;
 
 import java.io.File;
-import java.util.Objects;
 
 public class PaletteActivity extends AppCompatActivity {
 
     ImageView settingsButton;
-    ImageView givenImage;
+    View[] colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +25,46 @@ public class PaletteActivity extends AppCompatActivity {
 
         settingsButton = findViewById(R.id.paletteViewSettingsButton);
         settingsButton.setOnClickListener(settingsButtonListener());
-        givenImage = findViewById(R.id.exampleImg);
 
         // Get intent and given path from the camera intent
         Intent intent = getIntent();
-        String currentPhotoPath = Objects.requireNonNull(intent.getExtras()).getString("currentPhotoLocation");
 
-        // Make a File and get its URI to set the ImageView's src to
-        assert currentPhotoPath != null;
-        File f = new File(currentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
+        Bundle extras = intent.getExtras();
 
-        givenImage.setImageURI(contentUri);
+        colors = new View[6];
+
+        colors[0] = findViewById(R.id.palette1);
+        colors[1] = findViewById(R.id.palette2);
+        colors[2] = findViewById(R.id.palette3);
+        colors[3] = findViewById(R.id.palette4);
+        colors[4] = findViewById(R.id.palette5);
+        colors[5] = findViewById(R.id.palette6);
+
+        if (extras != null) {
+            if (extras.containsKey("currentPhotoLocation")) {
+                String currentPhotoPath = extras.getString("currentPhotoLocation");
+
+                assert currentPhotoPath != null;
+                Log.d("path", currentPhotoPath);
+
+                // Use preferred ARGB_8888 decoding for 4 byte pixels
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+                // Generate bitmap from the file path
+                File f = new File(currentPhotoPath);
+                Bitmap bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(), options);
+
+                Palette p = createPaletteSync(bitmap);
+                getColorsFromPalette(p);
+            }
+        }
+
+    }
+
+    // Generate palette synchronously and return it
+    public Palette createPaletteSync(Bitmap bitmap) {
+        return Palette.from(bitmap).generate();
     }
 
     private View.OnClickListener settingsButtonListener() {
@@ -45,4 +75,20 @@ public class PaletteActivity extends AppCompatActivity {
             }
         };
     }
+
+    // Get the colors from a given palette and assign them to a simple view
+    public void getColorsFromPalette(Palette p) {
+        int i = 0;
+
+        for (Palette.Swatch s : p.getSwatches()) {
+            if (i == 6) {
+                break;
+            }
+
+            colors[i].setBackgroundColor(s.getRgb());
+
+            i++;
+        }
+    }
+
 }
